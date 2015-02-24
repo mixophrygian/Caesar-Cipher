b3ad9eb4a383a52e6e666f4a5f6727ebe012c178
Eleanor Weigert <eweigert@gmail.com>
2015-02-24 08:49:55 -0800

    added a couple comments

--- Caesar.rb
+++ Caesar.rb
@@ -1,6 +1,6 @@
 def caesar_cipher (string, num)
 	phrase = string.split("")
-	shift = (num.to_i)%26
+	shift = (num.to_i)%26 #keeps shifts within the size of the alphabet
 	phrase.each do  |letter|
 		if letter =~ (/[A-zA-Z]/)
 				shift.times do
@@ -8,7 +8,7 @@ def caesar_cipher (string, num)
 				end				
 		end
 		if letter.length > 1
-			letter[0] = ''
+			letter[0] = '' #strings incremented beyond z or Z gain a second index which is the desired output. This lops off the first, now erroneous index.
 		end
 	end
 	puts "\""+ string + "\"" + " shifted by " + num.to_s + " becomes " + "\"" + phrase.join("") + "\""
